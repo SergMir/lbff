129d48a563bceb62a7f2d3d72c3fe3ad9b9b7f98
DartZerg <DartZerg@gmail.com>
2011-05-17 18:58:27 +0300

    MAJOR CHANGE: OpenCLization step 3

--- src/solver/solver_opencl.c
+++ src/solver/solver_opencl.c
@@ -15,28 +15,20 @@
       break; \
     }
 
-
-const char *source =
-  "__kernel void memset( __global uint *dst )                              \n"
-  "{                                                                       \n"
-  " dst[get_global_id(0)] = get_global_id(0);                              \n"
-  "} \n";
-
 const char *sourceLB_BHK =
   "__kernel void lb_bhk(__global float *us,                                  \n"
   "                     const __global float *fs,                            \n"
   "                     __global float *fsn,                                 \n"
   "                     const __global float *vectors,                       \n"
-  "                     const int nodes_cnt,                                 \n"
-  "                     const int vectors_cnt)                               \n"
+  "                     const  int nodes_cnt,                                \n"
+  "                     const  int vectors_cnt)                              \n"
   "{                                                                         \n"
   "  int i = get_global_id(0);                                               \n"
-  "  //for (i = 0; i < 90*90; ++i)                                         \n"
   "  {                                                                       \n"
   "    int j;                                                                \n"
-  "    for (j = 0; j < 9; ++j)                                     \n"
+  "    for (j = 0; j < vectors_cnt; ++j)                                     \n"
   "    {                                                                     \n"
-  "      fsn[i * vectors_cnt + j] = 2;//vectors[j];                              \n"
+  "      fsn[i * vectors_cnt + j] = vectors[j*4 + 3];                        \n"
   "    }                                                                     \n"
   "  }                                                                       \n"
   "} \n";
@@ -45,8 +37,6 @@ const char *sourceLB_BHK =
 cl_platform_id platform;
 cl_device_id device;
 cl_context context;
-cl_program program;
-cl_kernel kernel;
 
 cl_program program_lb_bhk;
 cl_kernel kernel_lb_bhk;
@@ -60,13 +50,15 @@ int solver_ResolveOpencl(LB_Lattice_p lattice)
   cl_command_queue queue;
   solver_vector_p vector = solver_GetVectors(lattice->node_type);
   cl_int status = CL_SUCCESS;
-  cl_event event;
   
   queue = clCreateCommandQueue(context,
                                device,
-                               0, NULL);
+                               0, &status);
+  if (CL_SUCCESS != status) { exit(-1); }
+  
   if (NULL == lattice->openCLparams)
   {
+    memset(lattice->fs + fs_size, 0, sizeof (lb_float) * fs_size);
     do
     {
       lattice->openCLparams = (LB_OpenCL_p) malloc(sizeof (LB_OpenCL_t));
@@ -84,9 +76,9 @@ int solver_ResolveOpencl(LB_Lattice_p lattice)
                                                  lattice->fs,
                                                  &status);
       solver_breakIfFailed;
-      
+
       lattice->openCLparams->fsnew = clCreateBuffer(context,
-                                                    CL_MEM_READ_WRITE | CL_MEM_USE_HOST_PTR,
+                                                    CL_MEM_WRITE_ONLY | CL_MEM_USE_HOST_PTR,
                                                     sizeof (lb_float) * fs_size,
                                                     lattice->fs + fs_size,
                                                     &status);
@@ -97,29 +89,31 @@ int solver_ResolveOpencl(LB_Lattice_p lattice)
                                                       sizeof (lb_float) * lattice->node_type,
                                                       vector,
                                                       &status);
+      solver_breakIfFailed;
+      
+      
     } while (0);
   }
   
+  
   if (CL_SUCCESS == status)
   {
-    /* 6. Launch the kernel. Let OpenCL pick the local work size */
     global_work_size = nodes_cnt;
-    clSetKernelArg(kernel, 0, nodes_cnt * sizeof (LB3D_t), (void*) lattice->openCLparams->u);
-    clSetKernelArg(kernel, 1, nodes_cnt * sizeof (LB3D_t), (void*) lattice->openCLparams->fs);
-    clSetKernelArg(kernel, 2, nodes_cnt * sizeof (LB3D_t), (void*) lattice->openCLparams->fsnew);
-    clSetKernelArg(kernel, 3, nodes_cnt * sizeof (LB3D_t), (void*) lattice->openCLparams->vectors);
-    clSetKernelArg(kernel, 4, sizeof (nodes_cnt), (void*) &nodes_cnt);
-    clSetKernelArg(kernel, 5, sizeof (lattice->node_type), (void*) &(lattice->node_type));
+    clSetKernelArg(kernel_lb_bhk, 0, sizeof(cl_mem), (void*) &(lattice->openCLparams->u));
+    clSetKernelArg(kernel_lb_bhk, 1, sizeof(cl_mem), (void*) &(lattice->openCLparams->fs));
+    clSetKernelArg(kernel_lb_bhk, 2, sizeof(cl_mem), (void*) &(lattice->openCLparams->fsnew));
+    clSetKernelArg(kernel_lb_bhk, 3, sizeof(cl_mem), (void*) &(lattice->openCLparams->vectors));
+    clSetKernelArg(kernel_lb_bhk, 4, sizeof(int),    (void*) &nodes_cnt);
+    clSetKernelArg(kernel_lb_bhk, 5, sizeof(int),    (void*) &(lattice->node_type));
     status = clEnqueueNDRangeKernel(queue,
-                           kernel,
+                           kernel_lb_bhk,
                            1,
                            NULL,
                            &global_work_size,
                            NULL, 0, NULL,
-                           &event);
+                           NULL);
     clFinish(queue);
 
-    status = clWaitForEvents(1, &event);
     status = clEnqueueReadBuffer(queue,
                                 (cl_mem)lattice->openCLparams->fsnew,
                                  CL_TRUE,
@@ -127,31 +121,12 @@ int solver_ResolveOpencl(LB_Lattice_p lattice)
                                  sizeof (lb_float) * fs_size,
                                  lattice->fs + fs_size,
                                  0, NULL, NULL);
-    /* 7. Look at the results via synchronous buffer map */
-    /*lattice->fs = (lb_float *) clEnqueueMapBuffer(queue,
-                                                 (cl_mem)lattice->openCLparams->fs,
-                                                 CL_TRUE,
-                                                 CL_MAP_READ,
-                                                 0,
-                                                 sizeof (lb_float) * fs_size,
-                                                 0,
-                                                 NULL,
-                                                 NULL,
-                                                 NULL);*/
-
-    {
-      int i;
-      for (i = 0; i < nodes_cnt; ++i)
-      {
-        if (lattice->fs[fs_size + i] > 0.0001)
-        {
-          lattice->fs[fs_size + i] *= 1.0;
-        }
-      }
-      memcpy(lattice->fs, lattice->fs + fs_size, fs_size * sizeof (lb_float));
-    }
   }
   
+  if (CL_SUCCESS != status)
+  {
+    exit(-1);
+  }
   return (CL_SUCCESS == status) ? 0 : -1;
 }
 
@@ -178,21 +153,6 @@ int solver_initOpencl(void)
                               &device,
                               NULL, NULL,
                               &status);
-    solver_breakIfFailed;
-
-    /* 4. Perform runtime source compilation, and obtain kernel entry point */
-    program = clCreateProgramWithSource(context,
-                                        1,
-                                        &source,
-                                        NULL,
-                                        &status);
-    solver_breakIfFailed;
-
-    status = clBuildProgram(program, 1, &device, NULL, NULL, NULL);
-    solver_breakIfFailed;
-
-    kernel = clCreateKernel(program, "memset",
-                            &status);
     solver_breakIfFailed;
 
     program_lb_bhk = clCreateProgramWithSource(context,
